127eb0d178df60dba67bb0d585d509bcf816758e
Alastair Crabtree <alastaircrabtree@gmail.com>
2016-11-28 00:34:03 +0000

    Comments

--- LazyCache/CachingService.cs
+++ LazyCache/CachingService.cs
@@ -6,10 +6,40 @@ namespace LazyCache
 {
     public class CachingService : IAppCache
     {
+        /// <summary>
+        /// Create an in-memory cache using the default instance of the MemoryCache. 
+        /// The default instance of MemoryCache is shared by default with all other instances of CachingService
+        /// Memory usage can be configured in a standard config file.
+        /// -----------------------------------------------------------------------------------------------------------------------------
+        /// cacheMemoryLimitMegabytes:   The amount of maximum memory size to be used. Specified in megabytes. 
+        ///                              The default is zero, which indicates that the MemoryCache instance manages its own memory
+        ///                              based on the amount of memory that is installed on the computer. 
+        /// physicalMemoryPercentage:    The percentage of physical memory that the cache can use. It is specified as an integer value from 1 to 100. 
+        ///                              The default is zero, which indicates that the MemoryCache instance manages its own memory 
+        ///                              based on the amount of memory that is installed on the computer. 
+        /// pollingInterval:             The time interval after which the cache implementation compares the current memory load with the 
+        ///                              absolute and percentage-based memory limits that are set for the cache instance.
+        ///                              The default is two minutes.
+        /// -----------------------------------------------------------------------------------------------------------------------------
+        ///  <configuration>
+        ///    <system.runtime.caching>
+        ///      <memoryCache>
+        ///        <namedCaches>
+        ///          <add name="default" cacheMemoryLimitMegabytes="0" physicalMemoryPercentage="0" pollingInterval="00:02:00" />
+        ///        </namedCaches>
+        ///      </memoryCache>
+        ///    </system.runtime.caching>
+        ///  </configuration>
+        /// </summary>
         public CachingService() : this(MemoryCache.Default)
         {
         }
 
+        /// <summary>
+        /// Create a cache using a specified object cache. 
+        /// CacheingService expects the consumer to manange the lifetime of the ObjectCache and does not dispose it
+        /// </summary>
+        /// <param name="cache">A cache implmentation such as MemoryCache</param>
         public CachingService(ObjectCache cache)
         {
             if (cache == null)
